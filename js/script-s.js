c9ce19c2e807302dc9bbc885e7bb9382c8a5c5f7
Sanjar <sanjarabdubositov011@gmail.com>
2025-03-29 21:46:56 +0500

    remove files

--- js/script-s.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    if (window.location.pathname.endsWith("pages/success.html")) {
-        window.history.pushState({}, "", "/success");
-    }   
-});
